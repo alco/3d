f953c505e00eb706e7158944e7c69484c66c0f7a
Alexei Sholik <alcosholik@gmail.com>
2012-06-30 14:49:34 +0300

    Grow large

--- cubism.js
+++ cubism.js
@@ -54,9 +54,14 @@ function initShaders(gl, ids) {
 
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.texcoordAttribute = gl.getAttribLocation(shaderProgram, "aTexCoord");
+    gl.enableVertexAttribArray(shaderProgram.texcoordAttribute);
+    shaderProgram.normalAttribute = gl.getAttribLocation(shaderProgram, "aNormal");
+    gl.enableVertexAttribArray(shaderProgram.normalAttribute);
 
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uProjMatrix");
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uModelViewMatrix");
+    shaderProgram.normMatrixUniform = gl.getUniformLocation(shaderProgram, "uNormalMatrix");
 
     return shaderProgram;
 }
@@ -79,74 +84,167 @@ function initGL(canvas) {
         return;
     }
 
-    gl.clearColor(0, 0, 0, 1);
+    gl.enable(gl.DEPTH_TEST);
 
     return gl;
 }
 
-function initBuffers(gl) {
-    var triangleVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    var vertices = [
-         0.0,  1.0,  0.0,
-        -1.0, -1.0,  0.0,
-         1.0, -1.0,  0.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    triangleVertexPositionBuffer.itemSize = 3;
-    triangleVertexPositionBuffer.numItems = 3;
+//
+// makeBox
+//
+// Create a box with vertices, normals and texCoords. Create VBOs for each as well as the index array.
+// Return an object with the following properties:
+//
+//  normalObject        WebGLBuffer object for normals
+//  texCoordObject      WebGLBuffer object for texCoords
+//  vertexObject        WebGLBuffer object for vertices
+//  indexObject         WebGLBuffer object for indices
+//  numIndices          The number of indices in the indexObject
+//
+function makeBox(ctx) {
+    // box
+    //    v6----- v5
+    //   /|      /|
+    //  v1------v0|
+    //  | |     | |
+    //  | |v7---|-|v4
+    //  |/      |/
+    //  v2------v3
+    //
+    // vertex coords array
+    var vertices = new Float32Array(
+        [  1, 1, 1,  -1, 1, 1,  -1,-1, 1,   1,-1, 1,    // v0-v1-v2-v3 front
+           1, 1, 1,   1,-1, 1,   1,-1,-1,   1, 1,-1,    // v0-v3-v4-v5 right
+           1, 1, 1,   1, 1,-1,  -1, 1,-1,  -1, 1, 1,    // v0-v5-v6-v1 top
+          -1, 1, 1,  -1, 1,-1,  -1,-1,-1,  -1,-1, 1,    // v1-v6-v7-v2 left
+          -1,-1,-1,   1,-1,-1,   1,-1, 1,  -1,-1, 1,    // v7-v4-v3-v2 bottom
+           1,-1,-1,  -1,-1,-1,  -1, 1,-1,   1, 1,-1 ]   // v4-v7-v6-v5 back
+    );
 
-    var squareVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-    vertices = [
-         1.0,  1.0,  0.0,
-        -1.0,  1.0,  0.0,
-         1.0, -1.0,  0.0,
-        -1.0, -1.0,  0.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    squareVertexPositionBuffer.itemSize = 3;
-    squareVertexPositionBuffer.numItems = 4;
+    // normal array
+    var normals = new Float32Array(
+        [  0, 0, 1,   0, 0, 1,   0, 0, 1,   0, 0, 1,     // v0-v1-v2-v3 front
+           1, 0, 0,   1, 0, 0,   1, 0, 0,   1, 0, 0,     // v0-v3-v4-v5 right
+           0, 1, 0,   0, 1, 0,   0, 1, 0,   0, 1, 0,     // v0-v5-v6-v1 top
+          -1, 0, 0,  -1, 0, 0,  -1, 0, 0,  -1, 0, 0,     // v1-v6-v7-v2 left
+           0,-1, 0,   0,-1, 0,   0,-1, 0,   0,-1, 0,     // v7-v4-v3-v2 bottom
+           0, 0,-1,   0, 0,-1,   0, 0,-1,   0, 0,-1 ]    // v4-v7-v6-v5 back
+       );
 
-    return [triangleVertexPositionBuffer, squareVertexPositionBuffer];
+
+    // texCoord array
+    var texCoords = new Float32Array(
+        [  1, 1,   0, 1,   0, 0,   1, 0,    // v0-v1-v2-v3 front
+           0, 1,   0, 0,   1, 0,   1, 1,    // v0-v3-v4-v5 right
+           1, 0,   1, 1,   0, 1,   0, 0,    // v0-v5-v6-v1 top
+           1, 1,   0, 1,   0, 0,   1, 0,    // v1-v6-v7-v2 left
+           0, 0,   1, 0,   1, 1,   0, 1,    // v7-v4-v3-v2 bottom
+           0, 0,   1, 0,   1, 1,   0, 1 ]   // v4-v7-v6-v5 back
+       );
+
+    // index array
+    var indices = new Uint8Array(
+        [  0, 1, 2,   0, 2, 3,    // front
+           4, 5, 6,   4, 6, 7,    // right
+           8, 9,10,   8,10,11,    // top
+          12,13,14,  12,14,15,    // left
+          16,17,18,  16,18,19,    // bottom
+          20,21,22,  20,22,23 ]   // back
+      );
+
+    var retval = { };
+
+    retval.normalObject = ctx.createBuffer();
+    ctx.bindBuffer(ctx.ARRAY_BUFFER, retval.normalObject);
+    ctx.bufferData(ctx.ARRAY_BUFFER, normals, ctx.STATIC_DRAW);
+
+    retval.texCoordObject = ctx.createBuffer();
+    ctx.bindBuffer(ctx.ARRAY_BUFFER, retval.texCoordObject);
+    ctx.bufferData(ctx.ARRAY_BUFFER, texCoords, ctx.STATIC_DRAW);
+
+    retval.vertexObject = ctx.createBuffer();
+    ctx.bindBuffer(ctx.ARRAY_BUFFER, retval.vertexObject);
+    ctx.bufferData(ctx.ARRAY_BUFFER, vertices, ctx.STATIC_DRAW);
+
+    retval.indexObject = ctx.createBuffer();
+    ctx.bindBuffer(ctx.ELEMENT_ARRAY_BUFFER, retval.indexObject);
+    ctx.bufferData(ctx.ELEMENT_ARRAY_BUFFER, indices, ctx.STATIC_DRAW);
+
+    retval.numIndices = indices.length;
+
+    return retval;
 }
 
 function setMatrixUniforms(gl, program, projMat, mvMat) {
     gl.uniformMatrix4fv(program.pMatrixUniform, false, projMat);
     gl.uniformMatrix4fv(program.mvMatrixUniform, false, mvMat);
+
+    normMat = mat4.create();
+    mat4.inverse(mvMat, normMat);
+    mat4.transpose(normMat);
+    gl.uniformMatrix4fv(program.normMatrixUniform, false, normMat);
 }
 
-
-function drawScene(gl, program, projMat, mvMat, buffers) {
-    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, projMat);
-
-    mat4.identity(mvMat);
-
-    mat4.translate(mvMat, [-1.5, 0.0, -7.0]);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers[0]);
-    gl.vertexAttribPointer(program.vertexPositionAttribute, buffers[0].itemSize, gl.FLOAT, false, 0, 0);
-    setMatrixUniforms(gl, program, projMat, mvMat);
-    gl.drawArrays(gl.TRIANGLES, 0, buffers[0].numItems);
-
-    mat4.translate(mvMat, [3.0, 0.0, 0.0]);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers[1]);
-    gl.vertexAttribPointer(program.vertexPositionAttribute, buffers[1].itemSize, gl.FLOAT, false, 0, 0);
-    setMatrixUniforms(gl, program, projMat, mvMat);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, buffers[1].numItems);
-}
+var interval;
 
 function startGL() {
     var canvas = document.getElementById("canvas");
     var gl = initGL(canvas);
     var program = initShaders(gl, ["vshader", "fshader"]);
-    var buffers = initBuffers(gl);
+
+    // Set some uniform variables for the shaders
+    gl.uniform3f(gl.getUniformLocation(program, "lightDir"), 0, 0, 1);
+
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clearColor(0, 0, 0, 1);
 
     var projMat = mat4.create();
+    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, projMat);
+
     var mvMat = mat4.create();
-    drawScene(gl, program, projMat, mvMat, buffers);
+    mat4.identity(mvMat);
+    mat4.translate(mvMat, [0, 0.0, -7.0]);
+
+    var angle = 0;
+
+    var buf = makeBox(gl);
+
+    // Set up all the vertex attributes for vertices, normals and texCoords
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf.vertexObject);
+    gl.vertexAttribPointer(program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf.normalObject);
+    gl.vertexAttribPointer(program.normalAttribute, 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf.texCoordObject);
+    gl.vertexAttribPointer(program.texcoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+    // Bind the index array
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buf.indexObject);
+
+    interval = setInterval(function() {
+        angle += .01;
+        mat4.identity(mvMat);
+        mat4.translate(mvMat, [0, 0, -7]);
+        mat4.rotateX(mvMat, angle);
+        mat4.rotateY(mvMat, angle*2);
+
+        drawBuffer(gl, program, projMat, mvMat, buf.numIndices);
+    }, 32);
 
     return gl;
+}
+
+function stop() {
+    clearInterval(interval);
+}
+
+
+function drawBuffer(gl, program, projMat, mvMat, numIndices) {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    //gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    //gl.vertexAttribPointer(program.vertexPositionAttribute, buffer.itemSize, gl.FLOAT, false, 0, 0);
+    setMatrixUniforms(gl, program, projMat, mvMat);
+    gl.drawElements(gl.TRIANGLES, numIndices, gl.UNSIGNED_BYTE, 0);
 }
